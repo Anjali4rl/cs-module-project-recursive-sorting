db96b64f9eaaf4a0a5c046cb4400f25d4bed6514
dawsonhammdev <hammsdesigns@gmail.com>
2020-06-16 16:47:58 -0500

    I have met MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,18 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
-    # Your code here
+def binary_search(array, element, start, end):
+    if start > end:
+        return -1
+  
+    mid = (start + end) // 2
+    if element == array[mid]:
+        return mid
+
+    if element < array[mid]:
+        return binary_search(array, element, start, mid-1)
+    else:
+        return binary_search(array, element, mid+1, end)
+
+
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -10,5 +22,5 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
+    pass
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,19 +1,36 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+def merge(left, right, merged):
 
-    # Your code here
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+      
+        # Sort each one and place into the result
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor+right_cursor]=left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+            
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+        
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
 
-
-    return merged_arr
+    return merged
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+    # The last array split
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    # Perform merge_sort recursively on both halves
+    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
 
-
-    return arr
+    # Merge each side together
+    return merge(left, right, arr.copy())
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
@@ -21,8 +38,8 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
